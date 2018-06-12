03242fe43e2273d655481d88afaf7ef330e3d8d8
Ilia Korvigo <ilia.korvigo@gmail.com>
2018-06-12 14:39:09 +0300

    Initial commit

---
+++ finalise.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python
+
+"""
+
+
+
+"""
+
+import os
+import re
+import json
+from typing import TypeVar, Callable, Mapping
+
+import click
+from fn import F
+
+from mutagen import parser
+
+association = re.compile('^[A-Z0-9]*(?::\d*){1,2}$')
+
+
+A = TypeVar('A')
+
+
+def rename_associations(mapping: Mapping[str, str], record: parser.Record) \
+        -> parser.Record:
+    """
+    Rename associations in all subrecords.
+    :param mapping:
+    :param record:
+    :return:
+    :raises ValueError: if there are missing/malformed entries at any level,
+    if the mapping is malformed or non-exhaustive.
+    """
+
+    if not (F(map, association.match) >> all)(mapping.values()):
+        raise ValueError(f'malformed association mapping for {record.protein}')
+
+    def check_mut(mut: parser.Mutation) -> parser.Mutation:
+        if not (mut.subrecs and all(mut.subrecs)):
+            raise ValueError(
+                f'malformed subrecord(s) in {record.protein}:{mut.id}'
+            )
+        return parser.Mutation(
+            mut.id, mut.start, mut.stop, mut.ref, mut.alt, mut.description,
+            list(map(F(check_subrec, mut.id), mut.subrecs))
+        )
+
+    def check_subrec(mutid: int, subrec: parser.SubRecord) -> parser.SubRecord:
+        if not (subrec.effects and all(subrec.effects)):
+            raise ValueError(
+                f'malformed effect(s) in {record.protein}:{mutid}:{subrec.id}'
+            )
+        effects = list(map(rename_assoc, subrec.effects))
+        return parser.SubRecord(subrec.id, subrec.description, effects)
+
+    def rename_assoc(effect: parser.Effect) \
+            -> parser.Effect:
+        try:
+            associations = [mapping[assoc] for assoc in effect.associations]
+            return parser.Effect(effect.cls, effect.level, effect.target,
+                                 associations)
+        except KeyError as err:
+            raise ValueError(
+                f'non-exhaustive association mapping in {record.protein}; {err}'
+            )
+
+    if not (record.mutations and all(record.mutations)):
+        raise ValueError(f'malformed mutations in {record.protein}')
+
+    return parser.Record(record.protein, list(map(check_mut, record.mutations)))
+
+
+def optval(validator: Callable[[A], bool], message: str, ctx, param: str,
+           value: A):
+    if not validator(value):
+        raise click.BadParameter(message, ctx=ctx, param=param)
+    return value
+
+
+@click.command('finalise', help=__doc__,
+               context_settings=dict(help_option_names=['-h', '--help']))
+@click.option('-i', '--input', required=True,
+              type=click.Path(exists=True, dir_okay=False, resolve_path=True))
+@click.option('-a', '--associations',
+              type=click.Path(exists=True, dir_okay=False, resolve_path=True))
+@click.option('-o', '--output', required=True,
+              type=click.Path(exists=False, dir_okay=False, resolve_path=True),
+              callback=F(optval, lambda v: not os.path.exists(v), 'output exists'))
+def finalise(input, associations, output):
+    print('hello')
+    try:
+        with open(associations) as buffer:
+            mappings = json.load(buffer)
+    except json.JSONDecodeError:
+        raise click.BadParameter("can't parse association mapping")
+    try:
+        print('hello')
+        with open(input) as buffer:
+            records = parser.parse(parser.cleanup(buffer))
+    except (ValueError, AttributeError):
+        raise click.BadParameter(
+            "can't parse records (this usually signifies a record-level error)"
+        )
+    finalised_records = [
+        rename_associations(mappings.get(rec.protein, {}), rec)
+        for rec in records
+    ]
+    with open(output, 'w') as dest:
+        for line in parser.write(finalised_records):
+            print(line, file=dest)
+
+
+if __name__ == '__main__':
+    finalise()
+
+
---
+++ mutagen/parser.py
@@ -0,0 +1,171 @@
+from typing import Iterator, Iterable, NamedTuple, List, Callable, Optional
+from itertools import chain, groupby, filterfalse, repeat
+import operator as op
+import re
+
+from fn import F
+
+
+CLASSES = frozenset(['ORG', 'CEL', 'PAT', 'PRO', 'INT', 'IND', 'ENZ', 'MIM',
+                     'LOC', 'TRA', 'CHA', 'CAR', 'UNK'])
+LEVELS = frozenset(['--', '-', '0', '+', '++', 'r'])
+
+MUTPREF = re.compile('^<(\d+)>')
+SUBPREF = re.compile('^\[(\d+)\]')
+EFFREF = re.compile('^>>')
+PREF = frozenset(['<', '[', '>'])
+MUTPATT = re.compile(
+    '<(?P<id>\w+)>\s*(?P<ref>\w+)\|(?P<start>\d+)-(?P<stop>\d+)\|(?P<sub>\w+)\s+(?P<text>.+)'
+)
+SUBRECPATT = re.compile('\[(?P<id>\d+)\]\s*(?P<text>.+)')
+
+
+Effect = NamedTuple('Effect', [
+    ('cls', str),
+    ('level', Optional[str]),
+    ('target', Optional[str]),
+    ('associations', List[str])
+])
+
+SubRecord = NamedTuple('SubRecord', [
+    ('id', int),
+    ('description', str),
+    ('effects', List[Optional[Effect]])
+])
+
+Mutation = NamedTuple('Mutation', [
+    ('id', int),
+    ('start', int),
+    ('stop', int),
+    ('ref', Optional[str]),
+    ('alt', Optional[str]),
+    ('description', str),
+    ('subrecs', List[Optional[SubRecord]])
+])
+
+Record = NamedTuple('Record', [
+    ('protein', str),
+    ('mutations', List[Optional[Mutation]])
+])
+
+
+def optionable(*exceptions):
+    def wrapper(f):
+        def opt(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except exceptions:
+                return None
+        return opt
+    return wrapper
+
+
+def breakby(condition, lines: Iterable[str]) -> Iterator[Iterator[str]]:
+    return (
+        F(filterfalse, op.itemgetter(0)) >> (map, op.itemgetter(1))
+    )(groupby(lines, condition))
+
+
+def separate(condition: Callable[[str], bool], sep: str,
+             lines: Iterable[str]) -> Iterator[str]:
+    """
+    Add a separator on condition
+    :param lines:
+    :return:
+    """
+    return chain.from_iterable(
+        (sep, line) if condition(line) else (line,) for line in lines
+    )
+
+
+def cleanup(handle: Iterable[str]) -> Iterable[str]:
+    """
+    Remove comments from a handle to be parsed
+    """
+    def start(line: str) -> Optional[int]:
+        candidates = list(
+        filterfalse(F(op.eq, -1), [line.find('!!!'), line.find('***')])
+        )
+        return min(candidates) if candidates else None
+
+    return (line[:start(line)] for line in handle)
+
+
+def parse(handle: Iterable[str]) -> List[Record]:
+    def parse_rec(lines: Iterator[str]) -> Record:
+        protein = next(lines)
+        mutations = (
+            F(separate, MUTPREF.match, '') >> (breakby, op.not_) >>
+            (map, parse_mut) >> list
+        )(lines)
+        return Record(protein, mutations)
+
+    @optionable(ValueError, AttributeError)
+    def parse_mut(lines: Iterator[str]) -> Mutation:
+        id_, ref, start, stop, alt, text = MUTPATT.match(next(lines)).groups()
+        ref, alt = map(lambda x: None if x.lower() == 'none' else x, [ref, alt])
+        start, stop = map(int, [start, stop])
+        subrecords = (
+            F(separate, SUBPREF.match, '') >> (breakby, op.not_) >>
+            (map, parse_subrec) >> list
+        )(lines)
+        return Mutation(int(id_), start, stop, ref, alt, text.strip(), subrecords)
+
+    @optionable(ValueError)
+    def parse_subrec(lines: Iterator[str]) -> SubRecord:
+        id_, text = SUBRECPATT.match(next(lines)).groups()
+        effects = map(parse_effect, lines)
+        return SubRecord(int(id_), text, list(effects))
+
+    @optionable(ValueError)
+    def parse_effect(line: str) -> Effect:
+        # there can be at most 3 blank positions
+        # TODO add validators
+        cls, level, target, associations, *_ = chain(
+            line.lstrip('>> ').split('|'), repeat('', 3)
+        )
+        if cls.upper() not in CLASSES:
+            raise ValueError
+        level, target = map(lambda x: x.strip('?') or None, [level, target])
+        return Effect(cls, level, target,
+                      list(filter(bool, associations.split(';'))))
+
+    return (
+        F(map, str.strip) >> (filter, bool) >>
+        (separate, lambda l: l[0] not in PREF, '') >> (breakby, op.not_) >>
+        (map, parse_rec) >> list
+    )(handle)
+
+
+def write(records: Iterable[Record]) -> Iterable[str]:
+
+    def indent(level: int, text: str) -> str:
+        return '\t' * level + text
+
+    def write_record(rec: Record) -> Iterable[str]:
+        yield rec.protein
+        yield from (F(map, write_mut) >> chain.from_iterable)(rec.mutations)
+
+    def write_mut(mut: Mutation) -> Iterable[str]:
+        position = f'{mut.ref}|{mut.start}-{mut.stop}|{mut.alt}'
+        yield indent(1, f'<{mut.id}> {position} {mut.description}')
+        yield from (F(map, write_subrec) >> chain.from_iterable)(mut.subrecs)
+
+    def write_subrec(subrec: SubRecord) -> Iterable[str]:
+        yield indent(2, f'[{subrec.id}] {subrec.description}')
+        yield from map(write_eff, subrec.effects)
+
+    def write_eff(eff: Effect) -> str:
+        assoc = ';'.join(eff.associations)
+        effect = '|'.join([eff.cls, eff.level, eff.target, assoc])
+        return indent(3, f">> {effect}")
+
+    return (F(map, write_record) >> chain.from_iterable)(records)
+
+
+if __name__ == '__main__':
+    raise RuntimeError
+
+
+
+
