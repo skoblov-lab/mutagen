d1f6ad820c1143665ce8ef922b7462350c805a70
Ilia Korvigo <ilia.korvigo@gmail.com>
2018-06-30 16:59:46 +0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mutagen',
+    version='',
+    packages=['mutagen'],
+    url='',
+    license='',
+    author='ilia',
+    author_email='',
+    description=''
+)
